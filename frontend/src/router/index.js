0d38fca774aec06f633ac03c59d1435c512d0cb4
KimHyeyeonn <85014114+KimHyeyeonn@users.noreply.github.com>
2024-05-02 15:35:55 +0900

    login vue 구현

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router'
+// router/index.js
+import { createRouter, createWebHistory } from 'vue-router';
+import LoginView from '../views/LoginView.vue';
+
+const routes = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginView
+  }
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-    
-    },
-    
-  ]
-})
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+});
 
-export default router
+export default router;
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div>
+    <h2>회원가입</h2>
+    <SignUpForm />
+  </div>
+</template>
+
+<script>
+import SignUpForm from './SignUpForm.vue';
+
+export default {
+  components: {
+    SignUpForm
+  }
+};
+</script>
