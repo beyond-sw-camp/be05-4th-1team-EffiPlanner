858420d0bcad17b7ab36c169c1fda25dd032edd9
hyeonjin-dot <guswls_2610@naver.com>
2024-05-07 11:37:38 +0900

    add sql - insert category(default)

--- backend/src/main/resources/database/data.sql
+++ backend/src/main/resources/database/data.sql
@@ -1,5 +1,13 @@
-insert into category values
-        (1, '업무'),
-        (2, '개인'),
-        (3, '긴급'),
-        (4, '기타');
+INSERT INTO category (category_id, category_name)
+SELECT * FROM (
+                  SELECT 1 AS id, '업무' AS name
+                  UNION ALL
+                  SELECT 2 AS id, '개인' AS name
+                  UNION ALL
+                  SELECT 3 AS id, '긴급' AS name
+                  UNION ALL
+                  SELECT 4 AS id, '기타' AS name
+              ) AS tmp
+WHERE NOT EXISTS (
+    SELECT 1 FROM category WHERE id IN (1, 2, 3, 4)
+);
